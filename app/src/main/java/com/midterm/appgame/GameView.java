df5f7e5e717dfac7fe8b82a171203de0ec9fefc1
fcthuongxaa10 <letruongson10a7@gmail.com>
2021-05-18 16:45:24 +0700

    Initial commit

---
+++ app/src/main/java/com/midterm/appgame/GameView.java
@@ -0,0 +1,53 @@
+package com.midterm.appgame;
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Point;
+import android.graphics.Rect;
+import android.view.Display;
+import android.view.View;
+import android.os.Handler;
+
+public class GameView extends View
+{
+
+    Handler handler;
+    Runnable runnable;
+    final  int UPDATE_MILIS=30;
+    Bitmap background;
+    Display display;
+    Point point;
+    int dWidth,dWeight;
+    Rect rect;
+    public GameView(Context context)
+    {
+        super(context);
+        handler = new Handler();
+        runnable = new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                invalidate();
+            }
+        };
+        background = BitmapFactory.decodeResource(getResources(),R.drawable.ic_launcher_background);
+        display = ((Activity)getContext()).getWindowManager().getDefaultDisplay();
+        point = new Point();
+        display.getSize(point);
+        dWidth = point.x;
+        dWeight = point.y;
+        rect = new Rect(0,0,dWidth,dWeight);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+       // canvas.drawBitmap(background,0,0,null);
+        canvas.drawBitmap(background,null,rect,null);
+        handler.postDelayed(runnable,UPDATE_MILIS);
+    }
+}
